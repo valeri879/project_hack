6c19817c970d1e2bcf6cc6bc485c2fd2bbc7d2f8
valeri879 <valeri.kharitonashvili1@gmail.com>
2016-05-23 12:57:18 +0400

    final დბ

--- db/fb_hack.sql
+++ db/fb_hack.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 23, 2016 at 08:20 AM
+-- Generation Time: May 23, 2016 at 10:56 AM
 -- Server version: 10.1.13-MariaDB
--- PHP Version: 7.0.5
+-- PHP Version: 5.6.20
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -28,9 +28,16 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `passwords` (
   `id` int(10) NOT NULL,
-  `UserName` varchar(55) NOT NULL,
-  `Password` varchar(25) NOT NULL
+  `UserName` varchar(50) CHARACTER SET utf8 DEFAULT NULL,
+  `Password` varchar(50) CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `passwords`
+--
+
+INSERT INTO `passwords` (`id`, `UserName`, `Password`) VALUES
+(15, 'valeri.kharitonashvili1@gmail.com', '08001037880Ab');
 
 --
 -- Indexes for dumped tables
@@ -50,7 +57,7 @@ ALTER TABLE `passwords`
 -- AUTO_INCREMENT for table `passwords`
 --
 ALTER TABLE `passwords`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
